801aee11ac291a5ee7604cb90101b39f0a7c07fe
Rohit <rgupta6205@gmail.com>
2024-10-29 12:38:55 +0530

    code

--- styles/constant.js
+++ styles/constant.js
@@ -1,16 +1,15 @@
-import { Dimensions } from "react-native"
+import { Dimensions } from "react-native";
 
 // export const { width, height } = Dimensions.get('screen')
-export const SCREEN_WIDTH = Dimensions.get('screen').width
-export const SCREEN_HEIGHT = Dimensions.get('screen').height
+export const SCREEN_WIDTH = Dimensions.get("screen").width;
+export const SCREEN_HEIGHT = Dimensions.get("screen").height;
 
-
-export const PRIMARY_COLOR = "#2b87b0"
-export const PRIMARY_COLOR_TRANSPARENT = "#2b87b022"
-export const SECONDARY_COLOR = "#838280"
-export const LIGHT = "#ffffff"
-export const DARK = "#020409"
-export const INFO_COLOR = "#54B4D3"
-export const SUCCESS_COLOR = "#14A44D"
-export const WARNING_COLOR = "#E4A11B"
-export const DANGER_COLOR = "#DC4C64"
+export const PRIMARY_COLOR = "#76885B";
+export const PRIMARY_COLOR_TRANSPARENT = "#C3D1C0";
+export const SECONDARY_COLOR = "#838280";
+export const LIGHT = "#ffffff";
+export const DARK = "#020409";
+export const INFO_COLOR = "#4B5A3D";
+export const SUCCESS_COLOR = "#14A44D";
+export const WARNING_COLOR = "#E4A11B";
+export const DANGER_COLOR = "#DC4C64";
