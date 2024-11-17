8b32055742f72d6759e6efc70a4a60bcca782a25
Mihir Kumar Mishra <mkm20170@gmail.com>
2024-11-17 09:56:03 +0530

    Changes

--- components/card/Clickablecard.jsx
+++ components/card/Clickablecard.jsx
@@ -6,10 +6,10 @@ import { spacing, typography, SCREEN_WIDTH, LIGHT } from "../../styles";
 export default function ClickableCard({
   item,
   handleViewDetails,
-  isProject = false,
+  isProject = false, 
 }) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={() => handleViewDetails(item)}>
       <Card
         style={[
           spacing.mv1,
@@ -19,16 +19,27 @@ export default function ClickableCard({
             marginHorizontal: 8,
           },
         ]}
-        onPress={() => handleViewDetails(item)}
       >
         <View
           style={{ flexDirection: "row", alignItems: "center", padding: 16 }}
         >
           <View style={{ flex: 1 }}>
-            <H6 style={[typography.textBold]}>{item.siteName || item.id}</H6>
-            <P style={{ fontSize: 14 }}>{item.location}</P>
-            <P style={{ fontSize: 14 }}>{item.date}</P>
-            <P style={{ fontSize: 14 }}>{item.amount}</P>
+            {isProject ? (
+              <>
+                <H6 style={[typography.textBold]}>{item.projectName}</H6>
+                <P style={{ fontSize: 14 }}>Duration: {item.duration}</P>
+                <P style={{ fontSize: 14 }}>Status: {item.status}</P>
+              </>
+            ) : (
+              <>
+                <H6 style={[typography.textBold]}>
+                  {item.siteName || item.id}
+                </H6>
+                <P style={{ fontSize: 14 }}>Location: {item.location}</P>
+                <P style={{ fontSize: 14 }}>Date: {item.date}</P>
+                <P style={{ fontSize: 14 }}>Amount: {item.amount}</P>
+              </>
+            )}
           </View>
         </View>
       </Card>
--- screens/RequisitionScreen.jsx
+++ screens/RequisitionScreen.jsx
@@ -9,10 +9,11 @@ import ClickableCard from "../components/card/Clickablecard";
 import { styles } from "../styles/components.styles";
 import { H2 } from "../components/text";
 import { typography } from "../styles";
-const RequisitionScreen = ({ navigation }) => {
-  const [searchText, setSearchText] = useState("");
 
-  const handleViewDetails = (item) => {
+
+const RequisitionScreen = ({ navigation }) => {
+const [searchText, setSearchText] = useState("");
+const handleViewDetails = (item) => {
     navigation.navigate("DetailScreen", { item });
   };
 
