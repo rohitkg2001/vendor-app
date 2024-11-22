da1467b15e03ca578f1cf5387dd62067828a2883
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2024-11-23 01:30:03 +0530

    Production code cleanup and multilingual support

--- components/MenuItem.jsx
+++ components/MenuItem.jsx
@@ -2,13 +2,15 @@ import { TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { H5 } from './text'
 import { typography, styles, spacing } from "../styles";
+import { useTranslation } from "react-i18next";
 
 export default function MenuItem({ label, icon, onPress }) {
+  const {t}=useTranslation()
   return (
     <TouchableOpacity style={[styles.rowItem, spacing.ph1, spacing.mh2]} onPress={onPress}>
       <View style={styles.rowBullet}>
         <Icon name={icon} size={24} style={typography.textDark} />
-        <H5 style={[typography.textDark, spacing.ph2]}>{label}</H5>
+        <H5 style={[typography.textDark, spacing.ph2]}>{t(label)}</H5>
       </View>
       <Icon name="chevron-forward-outline" size={24} color="#333" />
     </TouchableOpacity>
