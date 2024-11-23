843719f5955f97cdd39c234e675dacec7a40ba5c
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2024-11-24 00:22:44 +0530

    Production code cleanup with multilingual support

--- screens/InventoryScreen.jsx
+++ screens/InventoryScreen.jsx
@@ -7,6 +7,10 @@ import { inventory } from "../utils/faker";
 import InventoryCard from "../components/card/InventoryCard";
 import NoRecord from "./NoRecord";
 import { useTranslation } from "react-i18next";
+import Icon from "react-native-vector-icons/Ionicons";
+import Button from "../components/buttons/Button";
+import { ICON_MEDIUM, LIGHT, styles, spacing, SCREEN_WIDTH } from "../styles";
+import { View } from "react-native";
 
 export default function InventoryScreen() {
   const [searchText, setSearchText] = useState("");
@@ -20,11 +24,16 @@ export default function InventoryScreen() {
         data={inventory}
         keyExtractor={(item) => item.id.toString()}
         ListHeaderComponent={() => (
-          <SearchBar
-            value={searchText}
-            onChangeText={setSearchText}
-            style={{ marginVertical: 8, marginHorizontal: 4 }}
-          />
+          <View style={[styles.row, spacing.mh2, spacing.mb5, { alignItems: 'center' }]}>
+            <SearchBar
+              value={searchText}
+              onChangeText={setSearchText}
+              style={{ width: SCREEN_WIDTH - 70 }}
+            />
+            <Button style={[styles.btn, styles.bgPrimary, spacing.mh1, { width: 50 }]}>
+              <Icon name="options-outline" size={ICON_MEDIUM} color={LIGHT} />
+            </Button>
+          </View>
         )}
         ListEmptyComponent={() => (
           <NoRecord msg={t('no_inventory')} />
