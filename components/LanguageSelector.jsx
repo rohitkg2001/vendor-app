81127337730640d517f2200fd426da343b49f2ee
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2024-11-23 23:24:58 +0530

    Production code cleanup with multilingual support

--- components/LanguageSelector.jsx
+++ components/LanguageSelector.jsx
@@ -1,12 +1,10 @@
+import Icon from 'react-native-vector-icons/Ionicons'
+import { View } from "react-native";
 import Button from "../components/buttons/Button";
-import { styles } from "../styles/components.styles";
-import { H2, H4, H5, H6, P, Span } from "../components/text";
-import { typography, SCREEN_WIDTH, spacing, PRIMARY_COLOR } from "../styles";
+import { H2, H4, P, Span } from "../components/text";
 import ImageSource from '../assets/welcomeImage.png'
 import MyImageBackground from "./MyImageBackground";
-import Icon from 'react-native-vector-icons/Ionicons'
-import { ICON_SMALL } from "../styles/constant";
-import { View } from "react-native";
+import { typography, SCREEN_WIDTH, spacing, PRIMARY_COLOR, styles, ICON_SMALL } from "../styles";
 
 export default function LanguageSelector({ onSelectLanguage }) {
   return (
@@ -30,38 +28,18 @@ export default function LanguageSelector({ onSelectLanguage }) {
         <Icon name="checkmark-circle" size={ICON_SMALL} color={PRIMARY_COLOR} />
         <P style={{ lineHeight: 30 }}>Manage and track your tasks easily</P>
       </View>
-      <Span style={[typography.font16, spacing.mt5, spacing.mh2]}>
-        Please select your preferred language:
-      </Span>
+      <Span style={[typography.font16, spacing.mt5, spacing.mh2]}>Please select your preferred language:</Span>
       <Button
-        style={[
-          styles.btn,
-          styles.bgPrimary,
-          spacing.m1,
-          spacing.br5,
-          { width: SCREEN_WIDTH - 16, justifyContent: 'center' },
-        ]}
+        style={[styles.btn, styles.bgPrimary, spacing.m1, spacing.br5, { width: SCREEN_WIDTH - 16, justifyContent: 'center' },]}
         onPress={() => onSelectLanguage("en")}
       >
-        <H2 style={[styles.btnText, typography.font20, typography.textLight]}>
-          English
-        </H2>
+        <H2 style={[styles.btnText, typography.font20, typography.textLight]}>English</H2>
       </Button>
-
       <Button
-        style={[
-          styles.btn,
-          styles.bgLight,
-          spacing.m1,
-          spacing.bw2,
-          spacing.br5,
-          { width: SCREEN_WIDTH - 16, justifyContent: 'center', borderColor: PRIMARY_COLOR }
-        ]}
+        style={[styles.btn, styles.bgLight, spacing.m1, spacing.bw2, spacing.br5, { width: SCREEN_WIDTH - 16, justifyContent: 'center', borderColor: PRIMARY_COLOR }]}
         onPress={() => onSelectLanguage("hi")}
       >
-        <H2 style={[styles.btnText, typography.font20, typography.textPrimary]}>
-          हिंदी
-        </H2>
+        <H2 style={[styles.btnText, typography.font20, typography.textPrimary]}>हिंदी</H2>
       </Button>
     </MyImageBackground>
   );
