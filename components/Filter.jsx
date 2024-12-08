cc0d8c3556dab4acdbda58230af08873fb57cc3d
Rohit <rgupta6205@gmail.com>
2024-12-08 23:50:32 +0530

    setup reducer

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import BottomSheet from "./BottomSheet/BottomSheet";
+import BottomSheet from "./BottomSheet/Bottomsheet";
+
 import {
   SCREEN_WIDTH,
   spacing,
