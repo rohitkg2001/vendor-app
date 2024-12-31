af8e9fc4d50cfd529c28f9393b84bfbf97e5468a
Rohit <rgupta6205@gmail.com>
2024-12-31 16:14:45 +0530

    first commit

--- screens/DashboardScreen.jsx
+++ screens/DashboardScreen.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
-import { View, ScrollView, TouchableOpacity } from "react-native";
+import { View, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Ionicons";
 import ContainerComponent from "../components/ContainerComponent";
 import MyFlatList from "../components/utility/MyFlatList";
-import { H3, P } from "../components/text";
+import { H4, H5, P } from "../components/text";
 import StatCard from "../components/card/Statcard";
 import {
-  layouts,
-  PRIMARY_COLOR_TRANSPARENT,
-  DARK,
+  LIGHT,
+  PRIMARY_COLOR,
   spacing,
   styles,
   typography,
@@ -24,7 +23,7 @@ import Filter from "../components/Filter";
 import DashboardHeader from "../components/header/DashboardHeader";
 import DashboardFilter from "../components/filters/DashboardFilter";
 import CardsArray from "../components/card/CardsArray";
-
+import CardFullWidth from "../components/card/CardFullWidth";
 
 export default function DashboardScreen() {
   const [dueTasks, setDueTasks] = useState(4);
@@ -34,19 +33,24 @@ export default function DashboardScreen() {
   const navigation = useNavigation();
 
   const { firstName, id } = useSelector((state) => state.vendor);
-  const { tasks } = useSelector(state => state.tasks)
+  const { tasks } = useSelector((state) => state.tasks);
+
+  const [installation, setInstallation] = useState(0);
+  const [doneInstallation, setDoneInstallation] = useState(0);
+  const [rmsStatus, setRmsStatus] = useState(0);
+  const [donRMS, setDoneRMS] = useState(0);
 
   const { t } = useTranslation();
   const dispatch = useDispatch();
 
   useEffect(() => {
     setGreeting(greet());
-    dispatch(getAllTasks(id))
+    dispatch(getAllTasks(id));
   }, []);
 
   useEffect(() => {
-    setDueTasks(Array.isArray(tasks) ? tasks.length : 0)
-  }, [tasks, dispatch])
+    setDueTasks(Array.isArray(tasks) ? tasks.length : 0);
+  }, [tasks, dispatch]);
 
   const closeFilter = () => {
     setShowBottomSheet(!showBottomSheet);
@@ -54,13 +58,20 @@ export default function DashboardScreen() {
 
   return (
     <ContainerComponent>
-      <DashboardHeader dueTasks={dueTasks} greeting={greeting} firstName={firstName} navigation={navigation} notificationCount={dueTasks} />
+      <DashboardHeader
+        dueTasks={dueTasks}
+        greeting={greeting}
+        firstName={firstName}
+        navigation={navigation}
+        notificationCount={dueTasks}
+      />
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={[spacing.mh1]}
       >
         <DashboardFilter />
         {/* Flatlist cards */}
+
         <MyFlatList
           data={statCards}
           renderItem={({ item }) => (
@@ -80,6 +91,35 @@ export default function DashboardScreen() {
           numColumns={2}
           contentContainerStyle={spacing.mv4}
         />
+        <CardFullWidth backgroundColor={LIGHT}>
+          <View style={[styles.row, { alignItems: "center" }]}>
+            <Icon name="filter" size={ICON_LARGE} color={PRIMARY_COLOR} />
+            <H4 style={[typography.textBold, { marginRight: 110 }]}>
+              {t("Progress Report")}
+            </H4>
+          </View>
+          <View style={[spacing.bbw05, spacing.mv1]} />
+          <View
+            style={[
+              styles.row,
+              { justifyContent: "space-between", paddingVertical: 10 },
+            ]}
+          >
+            <View style={{ alignItems: "center" }}>
+              <P style={typography.textBold}>{t("Installation")}</P>
+              <H5 style={spacing.ml2}>
+                {doneInstallation}/
+                <H5 style={typography.textDanger}>{installation}</H5>
+              </H5>
+            </View>
+            <View style={{ alignItems: "center" }}>
+              <P style={typography.textBold}>{t("RMS")}</P>
+              <H5 style={spacing.ml2}>
+                {donRMS}/<H5 style={typography.textDanger}>{rmsStatus}</H5>
+              </H5>
+            </View>
+          </View>
+        </CardFullWidth>
         <CardsArray tasksCounts={tasksCounts} navigation={navigation} />
       </ScrollView>
       {showBottomSheet && (
