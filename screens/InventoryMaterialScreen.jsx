646dfc4c586fabea4ef918a0f2a627512476da30
Rohit <rgupta6205@gmail.com>
2025-04-02 02:16:40 +0530

    Add inventory

--- screens/InventoryMaterialScreen.jsx
+++ screens/InventoryMaterialScreen.jsx
@@ -1,14 +1,14 @@
-import { useCallback, useState } from 'react';
-import ContainerComponent from '../components/ContainerComponent'
+import { useCallback, useState } from "react";
+import ContainerComponent from "../components/ContainerComponent";
 import MyHeader from "../components/header/MyHeader";
-import SearchBar from '../components/input/SearchBar';
-import MyTabView from '../components/customtab/MyTabView';
-import { Text, View } from 'react-native';
-import { SceneMap } from 'react-native-tab-view';
+import SearchBar from "../components/input/SearchBar";
+import MyTabView from "../components/customtab/MyTabView";
+import { Text, View } from "react-native";
+import { SceneMap } from "react-native-tab-view";
 
 export default function InventoryMaterialScreen({ route, navigation }) {
   const { material } = route.params;
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const handleSearchChange = useCallback((text) => {
     setSearchText(text);
   }, []);
@@ -16,25 +16,35 @@ export default function InventoryMaterialScreen({ route, navigation }) {
   const inventoryTabs = [
     {
       key: 0,
-      title: 'Total Received',
+      title: "Total Received",
     },
     {
       key: 1,
-      title: 'In Stock',
-
+      title: "In Stock",
     },
     {
       key: 2,
-      title: 'Consumed',
+      title: "Consumed",
     },
   ];
 
   const renderScene = SceneMap({
-    0: () => <View><Text>Total Received</Text></View>,
-    1: () => <View><Text>In Stock</Text></View>,
-    2: () => <View><Text>Consumed</Text></View>,
-  })
-
+    0: () => (
+      <View>
+        <Text>Total Received</Text>
+      </View>
+    ),
+    1: () => (
+      <View>
+        <Text>In Stock</Text>
+      </View>
+    ),
+    2: () => (
+      <View>
+        <Text>Consumed</Text>
+      </View>
+    ),
+  });
 
   return (
     <ContainerComponent>
@@ -54,7 +64,11 @@ export default function InventoryMaterialScreen({ route, navigation }) {
         value={searchText}
         onChangeText={handleSearchChange}
         style={{ marginHorizontal: 16 }} /> */}
-      <MyTabView renderScene={renderScene} tabs={inventoryTabs} handleIndexChange={(index) => console.log(index)} />
+      <MyTabView
+        renderScene={renderScene}
+        tabs={inventoryTabs}
+        handleIndexChange={(index) => console.log(index)}
+      />
     </ContainerComponent>
-  )
+  );
 }
