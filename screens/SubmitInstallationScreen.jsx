05419714a57771adf2dc0d58958c7910be59ab19
Rohit <rgupta6205@gmail.com>
2025-04-15 12:23:27 +0530

    Add inventory

--- screens/SubmitInstallationScreen.jsx
+++ screens/SubmitInstallationScreen.jsx
@@ -1,3 +1,4 @@
+// import react native
 import React, { useState, useEffect } from "react";
 import { ScrollView, View, TouchableOpacity, Alert } from "react-native";
 import { Snackbar } from "react-native-paper";
