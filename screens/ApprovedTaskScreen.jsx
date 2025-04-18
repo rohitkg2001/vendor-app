d72c00b4c81968756a3de1245df66d21092ca7e6
Rohit <rgupta6205@gmail.com>
2025-04-18 12:29:46 +0530

    productuion

--- screens/ApprovedTaskScreen.jsx
+++ screens/ApprovedTaskScreen.jsx
@@ -1,3 +1,4 @@
+// import react native
 import { ScrollView, TouchableOpacity, View, Image } from "react-native";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
