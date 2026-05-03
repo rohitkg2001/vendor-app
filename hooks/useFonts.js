efdb9132ffa84072367fc3acec29a6e14a5716b4
sumit245 <sumitranjan245@gmail.com>
2026-05-03 21:16:57 +0530

    Message

--- hooks/useFonts.js
+++
@@ -1,6 +0,0 @@
-import * as Font from 'expo-font';
-
-export default useFonts = async () =>
-    await Font.loadAsync({
-        lato_regular: require('../assets/fonts/lato_regular.ttf')
-    });
--- redux/actions/index.js
+++
@@ -1,7 +0,0 @@
-import { getAllProjects } from "./projectActions";
-import { getAllSites } from "./siteActions";
-import { getAllTasks } from "./taskActions";
-import { getAllItems } from "./inventoryActions";
-import { download } from "./taskActions";
-
-export { getAllProjects, getAllSites, getAllTasks, getAllItems, download };
