10ec8c77e2af73d7c0d9f37cb1e1f23e215504af
sumit245 <sumitranjan245@gmail.com>
2026-05-03 21:15:30 +0530

    Message

--- components/buttons/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { TouchableOpacity } from 'react-native'
-
-export default function Button({ style, onPress, children }) {
-    return (
-        <TouchableOpacity style={style} onPress={onPress}>
-            {children}
-        </TouchableOpacity>
-    )
-}
